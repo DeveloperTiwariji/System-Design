0138401179f6cd7c36177e3e86daa1adb10d4daf
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2026-03-08 15:47:17 +0530

    Switch and Bulb example

--- Design_Principle/DIPExample.java
+++ Design_Principle/DIPExample.java
@@ -44,3 +44,83 @@ public class DIPExample {
 }
 
 
+
+// problme =====================
+
+
+// class Bulb{
+//     void turnOn(){
+//         System.out.println("Bulb is On ");
+//     }
+// }
+
+
+// class  Switch{
+//     Bulb bulb = new Bulb();
+    
+//     void operate(){
+//         bulb.turnOn();
+//     }
+// }
+
+// with DIP=======================
+
+interface Device{
+    void turnOn();
+    void turnOff();
+}
+
+
+
+class Bulb implements Device{
+    
+    public void turnOn(){
+        System.out.println("Bulb is ON");
+    }
+
+    public void turnOff(){
+        System.out.println("Bulb is OFF");
+    }
+}
+
+
+
+class Fan implements Device{
+
+    public void turnOn(){
+        System.out.println("Fan is ON");
+    }
+
+    public void turnOff(){
+        System.out.println("Fan is OFF");
+    }
+}
+
+
+class Switch{
+    Device device;
+
+    Switch(Device device){
+        this.device = device;
+    }
+
+
+    void operate(){
+        device.turnOn();
+        device.turnOff();
+    }
+}
+
+
+// inside main method===
+/*
+Bulb bulb = new Bulb();
+Switch s = new Switch(bulb);
+s.operat();
+
+Fan f = new Fan();
+Switch s = new Switch(f);
+s.operate();
+
+ */
+
